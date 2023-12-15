3f19ea17025352e3613f7790ff241ca76646a318
Angelo Theodorou <encelo@gmail.com>
2023-12-15 11:02:32 +0100

    Update GLFW to 3.3.9

--- cmake/glfw.cmake
+++ cmake/glfw.cmake
@@ -1,6 +1,6 @@
 set(TARGET_GLFW glfw)
-set(URL_GLFW https://github.com/glfw/glfw/releases/download/3.3.8/glfw-3.3.8.zip)
-set(URL_MD5_GLFW 4d607517a06c9773bb4b4dba0ab49a9b)
+set(URL_GLFW https://github.com/glfw/glfw/releases/download/3.3.9/glfw-3.3.9.zip)
+set(URL_MD5_GLFW d265c845628f0a85eef4c1f96b25355f)
 set(LIBNAME_GLFW glfw3)
 set(COMMON_CMAKE_ARGS_GLFW -DBUILD_SHARED_LIBS=ON -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF)
 
