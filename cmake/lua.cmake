939933689be993f66efe7891f024bacd4f548431
Angelo Theodorou <encelo@gmail.com>
2026-03-30 20:15:00 +0200

    Update Lua and libpng
    - Update Lua to 5.5.0
    - Update libpng to 1.6.56

--- cmake/lua.cmake
+++ cmake/lua.cmake
@@ -1,6 +1,6 @@
 set(TARGET_LUA lua)
-set(URL_LUA https://www.lua.org/ftp/lua-5.4.8.tar.gz)
-set(URL_MD5_LUA 81cf5265b8634967d8a7480d238168ce)
+set(URL_LUA https://www.lua.org/ftp/lua-5.5.0.tar.gz)
+set(URL_MD5_LUA c8b73c52a8c3c80978512f17347ee24d)
 set(LIBNAME_LUA lua)
 set(PROJECT_SRC_LUA ${EP_BASE}/Source/project_${TARGET_LUA})
 set(PROJECT_BUILD_LUA ${EP_BASE}/Build/project_${TARGET_LUA})
@@ -12,8 +12,8 @@ if(MSVC)
 		PATCH_COMMAND ${CMAKE_COMMAND} -E copy_if_different ${CMAKE_SOURCE_DIR}/patches/CMakeLists_lua.txt ${PROJECT_SRC_LUA}/CMakeLists.txt
 		BUILD_COMMAND ${CMAKE_COMMAND} --build . --config ${CMAKE_BUILD_TYPE} --parallel
 		BUILD_IN_SOURCE 0
-		INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_if_different ${PROJECT_BUILD_LUA}/${CMAKE_BUILD_TYPE}/lua54.dll ${BINDIR}/
-			COMMAND ${CMAKE_COMMAND} -E copy_if_different ${PROJECT_BUILD_LUA}/${CMAKE_BUILD_TYPE}/lua54.lib ${LIBDIR}/
+		INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_if_different ${PROJECT_BUILD_LUA}/${CMAKE_BUILD_TYPE}/lua55.dll ${BINDIR}/
+			COMMAND ${CMAKE_COMMAND} -E copy_if_different ${PROJECT_BUILD_LUA}/${CMAKE_BUILD_TYPE}/lua55.lib ${LIBDIR}/
 			COMMAND ${CMAKE_COMMAND} -E copy_if_different ${PROJECT_BUILD_LUA}/${CMAKE_BUILD_TYPE}/lua.lib ${LIBDIR}/
 			COMMAND ${CMAKE_COMMAND} -E copy_if_different ${PROJECT_BUILD_LUA}/${CMAKE_BUILD_TYPE}/lua.exe ${BINDIR}/
 			COMMAND ${CMAKE_COMMAND} -E copy_if_different ${PROJECT_BUILD_LUA}/${CMAKE_BUILD_TYPE}/luac.exe ${BINDIR}/
@@ -25,7 +25,7 @@ if(MSVC)
 	)
 elseif(APPLE)
 	set(FRAMEWORK_DIR_LUA ${DESTINATION_PATH}/${TARGET_LUA}.framework)
-	set(DYLIBNAME_LUA liblua5.4.dylib)
+	set(DYLIBNAME_LUA liblua5.5.dylib)
 
 	ExternalProject_Add(project_${TARGET_LUA}
 		URL ${URL_LUA}
@@ -81,8 +81,8 @@ elseif(MINGW)
 		CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
 		BUILD_COMMAND ${CMAKE_COMMAND} --build . --parallel
 		BUILD_IN_SOURCE 0
-		INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_if_different ${PROJECT_BUILD_LUA}/liblua54.dll ${DESTINATION_PATH}/bin/liblua54.dll
-			COMMAND ${CMAKE_COMMAND} -E copy_if_different ${PROJECT_BUILD_LUA}/liblua54.dll.a ${DESTINATION_PATH}/lib/liblua54.dll.a
+		INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_if_different ${PROJECT_BUILD_LUA}/liblua55.dll ${DESTINATION_PATH}/bin/liblua55.dll
+			COMMAND ${CMAKE_COMMAND} -E copy_if_different ${PROJECT_BUILD_LUA}/liblua55.dll.a ${DESTINATION_PATH}/lib/liblua55.dll.a
 			COMMAND ${CMAKE_COMMAND} -E copy_if_different ${PROJECT_BUILD_LUA}/liblua.a ${DESTINATION_PATH}/lib/liblua.a
 			COMMAND ${CMAKE_COMMAND} -E copy_if_different ${PROJECT_BUILD_LUA}/lua.exe ${DESTINATION_PATH}/bin/lua.exe
 			COMMAND ${CMAKE_COMMAND} -E copy_if_different ${PROJECT_BUILD_LUA}/luac.exe ${DESTINATION_PATH}/bin/luac.exe
@@ -100,7 +100,7 @@ else()
 		CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
 		BUILD_COMMAND ${CMAKE_COMMAND} --build . --parallel
 		BUILD_IN_SOURCE 0
-		INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_if_different ${PROJECT_BUILD_LUA}/liblua5.4.so ${DESTINATION_PATH}/lib/liblua5.4.so
+		INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_if_different ${PROJECT_BUILD_LUA}/liblua5.5.so ${DESTINATION_PATH}/lib/liblua5.5.so
 			COMMAND ${CMAKE_COMMAND} -E copy_if_different ${PROJECT_BUILD_LUA}/liblua.a ${DESTINATION_PATH}/lib/liblua.a
 			COMMAND ${CMAKE_COMMAND} -E copy_if_different ${PROJECT_BUILD_LUA}/lua ${DESTINATION_PATH}/bin/lua
 			COMMAND ${CMAKE_COMMAND} -E copy_if_different ${PROJECT_BUILD_LUA}/luac ${DESTINATION_PATH}/bin/luac
